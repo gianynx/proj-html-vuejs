66da5d81ddfedc5be1fc3c31d37ea720b28aaa74
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-26 11:52:24 +0200

    create project "Avada Charity"

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template></template>
+
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  font-family: 'Montserrat', sans-serif;
+}
+</style>
